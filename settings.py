664eecb0c9d1b0069b69407a8f187fa1bd165099
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-11-14 01:07:22 +0400

    Creating Bullet Class

--- settings.py
+++ settings.py
@@ -7,3 +7,9 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (155,190,200)
+
+        #Bullet Settings:
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullent_height = 15
+        self.bullet_color = (60,60,60)
