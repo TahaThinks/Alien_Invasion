b7c9cbfbdef3efba5710d8a0a34b3350445e88ee
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-11-19 01:18:44 +0400

    Moving the Aliens Right

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,7 +30,9 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
+            
 
     def _check_events(self):
         """Respond to keypresses and mouse events"""
@@ -106,6 +108,10 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     
     def _update_screen(self):
